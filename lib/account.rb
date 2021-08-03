9561f939a6ee4871c795e5dc2c0eeeef125bed91
Camilla <camilla.lima000@gmail.com>
2021-08-03 14:58:02 +0100

    added deposit method and made test pass

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,15 @@
 class Account
   
+  def initialize(balance = 0 )
+    @balance = balance
+    
+  end
+
+  def get_balance
+    @balance
+  end
+
+  def deposit(amount)
+   @balance = @balance + amount
+ end
 end
