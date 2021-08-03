f6a6c004699dc472f58d7e3df71213f77f47e310
Camilla <camilla.lima000@gmail.com>
2021-08-03 14:57:13 +0100

    added test for deposit feature

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,18 @@
 require 'account'
 
 describe Account do
+  
   it "starts account balance at 0" do
-    account = Account.new(0)
-    expect(account.balance).to eq(0)
+     account = Account.new(0)
+    expect(account.get_balance).to eq(0)
   end
+
+  it 'allows to deposit money in the account' do 
+     account = Account.new(0)
+    account.deposit(10)
+    expect(account.get_balance).to eq(10)
+  end
+
+
+  
 end
