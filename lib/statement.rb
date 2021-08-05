86b8796473621a73b2e96e28699bc9da4c7f956d
Camilla <camilla.lima000@gmail.com>
2021-08-05 17:29:50 +0100

    Separeted Statment from Account class

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,25 +1,15 @@
-require_relative 'account'
+# frozen_string_literal: true
+
+require 'account'
 
 class Statement
-
-  def initialize (date, balance_array, deposit_array, withdraw_array)
-    @date = date
-    @balance_array = balance_array
-    @deposit_array = deposit_array
-    @withdraw_array = withdraw_array
-    @money = -1
-  end
-
-  def print_title
-    "date || deposit || withdraw || balance"
-  end
-
   def print_statement
-    # puts "Your current balance is #{@balance}"
-     puts "date || deposit || withdraw || balance"
-    while @money > -1 -  @balance_array.length
-      puts "#{@date[@money]} || #{@deposit_array[@money]} || #{@withdraw_array[@money]} || #{@balance_array[@money]}"
-      @money -=1
+    
+    i = 0
+    while i <= @transactions.length
+      puts @print_title
+      puts @transactions[i]
+      i += 1
     end
   end
 end
