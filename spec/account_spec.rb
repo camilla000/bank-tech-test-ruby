078ac9019cd860e36a1322a1337457f27c71d281
Camilla <camilla.lima000@gmail.com>
2021-08-05 17:26:50 +0100

    Refactored account rspec tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,27 +1,50 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start
 require 'account'
-require 'date'
 
 describe Account do
-
- 
+  before(:each) do
+    @account = Account.new
+  end
+  before(:all) do
+    @transactions = []
+  end
 
   it 'starts account balance at 0' do
-    account = Account.new(0)
-    expect(account.get_balance).to eq(0)
+    expect(@account.balance).to eq 'Your current balance is £0'
   end
 
-  it 'allows to deposit money in the account' do
-    account = Account.new(0)
-    account.deposit(10)
-    expect(account.get_balance).to eq(10)
+  context '#deposit' do
+    it 'allows to deposit money in the account' do
+      @account.deposit(10)
+      expect(@account.balance).to eq 'Your current balance is £10'
+    end
   end
 
-  it 'allows to withdraw money the account' do
-    account = Account.new(0)
-    account.deposit(10)
-    account.withdraw(7)
-    expect(account.get_balance).to eq(3)
+  context 'withdraw' do
+    it 'allows to withdraw money from account' do
+      @account.deposit(10)
+      @account.withdraw(5)
+      expect(@account.balance).to eq 'Your current balance is £5'
+    end
+
+    it 'raises an error no enough money in account' do
+      expect { @account.withdraw(50) }.to raise_error "You don't have enough funds"
+    end
   end
 
-  
+  context '#date' do
+    it 'checks if its the actual date' do
+      expect(@account.date).to eq(Date.today)
+    end
+  end
+
+  context '#print_statement' do
+    it 'saves account deposit transactions' do
+      @account.deposit(100)
+      expect(@account.transactions).to eq([["#{Date.today}||     ||100||100"]])
+    end
+  end
 end
